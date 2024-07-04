8e290c132fbba8e0d49692902568f2ee922861f0
Sverdlik Gleb <97122264+Incomus@users.noreply.github.com>
2024-07-04 12:33:52 +0300

    Update 1_start.py

--- 1_start.py
+++ 1_start.py
@@ -6,7 +6,7 @@ import subprocess
 import sys
 
 
-event = True
+event = True # if event is running it's generally prefered to farm that event instead
 if len(sys.argv) > 1:
     week_schedule = sys.argv[1]
 else:
@@ -52,13 +52,15 @@ def general_end():
     subprocess.run(["python", "-m", "arknights-auto-macro.game.base.exit"], env=env)
     tm.sleep(1)
     weekday = datetime.datetime.now().weekday()
+    # this is a little unreliable. my time zone is switching arknights day at 2 pm and neither do I have any actions between 12 am - 1 am
+    # god (chatgpt) help you if your time zone is switching at 12 am or you want to run between 12 to 1 am
     if datetime.time(1, 00) <= datetime.datetime.now().time() <= datetime.time(14, 00):
         weekday = weekday - 1
         if weekday < 0:
             weekday = 6
-    if event == True:
+    if event == True: # if event is running it's generally prefered to farm that event instead
         weekday = 777
-    if weekday in [0, 3, 5, 6]:
+    if weekday in [0, 3, 5, 6]: # setup your own schedule here
         subprocess.run(["python", "-m", "arknights-auto-macro.game.farm.RC_120"], env=env)
         tm.sleep(1)
     elif weekday == 1:
@@ -73,7 +75,9 @@ def general_end():
     elif weekday == 777:
         subprocess.run(["python", "-m", "arknights-auto-macro.game.farm.event"], env=env)
         tm.sleep(1)
-    if datetime.time(1, 00) <= datetime.datetime.now().time() <= datetime.time(14, 00):
+    # new arknights day starts for me at 14 am, at 2 am I run last farm of the day
+    # as such it is better to run all farms and then collect mission for their better completion
+    if datetime.time(1, 00) <= datetime.datetime.now().time() <= datetime.time(14, 00): 
         subprocess.run(["python", "-m", "arknights-auto-macro.game.main.credit"], env=env)
         tm.sleep(1)
         subprocess.run(["python", "-m", "arknights-auto-macro.game.main.mission"], env=env)
@@ -118,7 +122,7 @@ def work():
     tm.sleep(1)
     general_end()
 
-def rest():
+def rest(): # this should run 6 hours after work out
     subprocess.run(["python", "-m", "arknights-auto-macro.record_start"], env=env)
     tm.sleep(1)
     subprocess.run(["python", "-m", "arknights-auto-macro.start"], env=env)
